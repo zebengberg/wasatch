815238270b653aee6f5c8fc593c9bde2d9cf1e19
Zeb Engberg <zebengberg@gmail.com>
2020-03-05 19:05:24 -0700

    added several new function, increased delay

--- arduino/led_strip/led_strip.ino
+++ arduino/led_strip/led_strip.ino
@@ -7,7 +7,7 @@ CRGB leds[NUM_LEDS];  // object from FastLED library
 
 
 // Parameters for switching which function to run and getting button input
-#define NUM_STATES 3
+#define NUM_STATES 5
 bool isButtonFirstPressed = true;  // used to get first button press
 unsigned long start_time = 0;
 byte state = 0;
@@ -17,14 +17,14 @@ byte j = 0;
 byte k = 0;
 
 // Parameters for bounce()
-double x1 = random(0, 35);
+double x1 = random(35);
 double x2 = random(40, 75);
 double x3 = random(80, 115);
 double x4 = random(120, 145);
-double dx1 = random(0, 100) / 100.0 - 0.5;
-double dx2 = random(0, 100) / 100.0 - 0.5;
-double dx3 = random(0, 100) / 100.0 - 0.5;
-double dx4 = random(0, 100) / 100.0 - 0.5;
+double dx1 = random(100) / 100.0 - 0.5;
+double dx2 = random(100) / 100.0 - 0.5;
+double dx3 = random(100) / 100.0 - 0.5;
+double dx4 = random(100) / 100.0 - 0.5;
 double color = 0;
 bool isColorIncreasing = true;
 
@@ -34,6 +34,13 @@ double dx = 0;
 int currentStep = 140;
 bool isMovingDown = true;
 
+// Parameters for math()
+double t = 0;
+
+// Parameters for randomFill()
+bool isOn[NUM_LEDS] = { false };
+int numOn = 0;
+
 
 void setup() {
   randomSeed(analogRead(0));
@@ -41,7 +48,7 @@ void setup() {
 }
 
 void loop() {
-  setState();
+  changeState();
 
   switch(state) {
     case 0:
@@ -53,29 +60,42 @@ void loop() {
     case 2:
       bounce();
       break;
+    case 3:
+      math();
+      break;
+    case 4:
+      randomFill();
+      break;
   }
 }
 
 // Change state every minute or when button is pressed
-void setState() {
+void changeState() {
   // Checking if a minute has elapsed 
   if (millis() > start_time + 1000UL * 60UL) {
     start_time = millis();
-    state++;
-    state %= NUM_STATES;
+    setRandomState();
     return;  // early exit
   }
   // Checking if button is pressed
   if (digitalRead(BUTTON_PIN)) {
     if (isButtonFirstPressed) {
       start_time = millis();
-      state++;
-      state %= NUM_STATES;
+      setRandomState();
       isButtonFirstPressed = false;
     }
   } else {
     isButtonFirstPressed = true;
   }
+}
+
+// Set the value of state to a random different value
+void setRandomState() {
+  byte r = random(0, NUM_STATES - 1);
+  if (r >= state) {
+    r++;
+  }
+  state = r;
 }
 
 
@@ -98,7 +118,7 @@ void rainbow() {
     k++;
   }
   FastLED.show();
-  delay(5);
+  delay(10);
 }
 
 
@@ -156,7 +176,7 @@ void bounce() {
     leds[i].b = 0;
   }
   FastLED.show();
-  delay(5);
+  delay(10);
 }
 
 // Object bouncing down steps
@@ -182,7 +202,7 @@ void stepper() {
     isMovingDown ? currentStep -= 20 : currentStep += 20;
 
   } else {
-    dx -= 0.01;
+    dx -= 0.02;
   }
   x += dx;
   
@@ -199,10 +219,65 @@ void stepper() {
     leds[i].b = color;
   }
   FastLED.show();
-  delay(5);
+  delay(10);
 }
 
 // Showing parametric curves involving sine and cosine
 void math() {
-  // TODO: write this
+  // Updating time
+  t += .01;
+  
+  // Clearing the strip
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].r = 0;
+    leds[i].g = 100;
+    leds[i].b = 100;
+  }
+  
+  // Showing a dot for sine
+  int y = NUM_LEDS / 2 + NUM_LEDS * sin(t) / 2;
+  leds[y].r = 255;
+  leds[y].g = 0;
+  leds[y].b = 0;
+
+  // Showing another dot for cosine
+  y = NUM_LEDS / 2 + NUM_LEDS * cos(t) / 2;
+  leds[y].r = 255;
+  leds[y].g = 200;
+  leds[y].b = 0;
+  FastLED.show();
+  delay(10);
+}
+
+
+// Randomly fill in the empty LED strip
+void randomFill() {
+  // If completely full, reset
+  if (numOn == NUM_LEDS) {
+    isOn[NUM_LEDS] = { false };
+    numOn = 0;
+    FastLED.clear();
+  }
+
+  // Getting random led to illuminate
+  int r = random(NUM_LEDS - numOn);
+  int i = 0;
+  while (r > 0) {
+    if (!isOn[i]) {
+      r--;
+    }
+    i++;
+  }
+  i--;  // incremented i too many times
+
+  // Updating globals
+  isOn[i] = true;
+  numOn++;
+
+  // Drawing  
+  leds[i].r = 200;
+  leds[i].g = 200;
+  leds[i].b = 100;
+  FastLED.show();
+  delay(10);
 }
