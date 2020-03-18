7f97e9caac03578a2dc3e789212481429909251e
Zeb Engberg <zebengberg@gmail.com>
2020-03-17 19:37:57 -0600

    update position working correctly

--- javascript/canvas_animations/spin.js
+++ javascript/canvas_animations/spin.js
@@ -1,7 +1,9 @@
 // Getting html elements.
+const WIDTH = window.innerWidth - 20;
+const HEIGHT = window.innerHeight - 40;
 const canvas = document.getElementById('myCanvas');
-canvas.width = window.innerWidth - 10;
-canvas.height = window.innerHeight - 40;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 const c = canvas.getContext('2d');
 
 
@@ -16,33 +18,54 @@ class Vector {
   plus(v2) { return new Vector(this.x + v2.x, this.y + v2.y); }
   minus(v2) { return new Vector(this.x - v2.x, this.y - v2.y); }
   dot(v2) { return this.x * v2.x + this.y * v2.y; }
+  det(v2) { return this.x * v2.y - this.y * v2.x; }
   rotate(theta) {
     const x = this.x * Math.cos(theta) - this.y * Math.sin(theta);
-    const y = this.x * Math.sin(theta) + this.y + Math.cos(theta);
+    const y = this.x * Math.sin(theta) + this.y * Math.cos(theta);
     return new Vector(x, y);
   }
 }
 
 
-class Polygon {
-  constructor(vertices) {
-    this.setRandomColor();
+class Polygon extends Array {
+  constructor(vertices = Polygon.buildVertices()) {
+    super(...vertices);
+    this.n = this.length;
+    this.push(vertices[0]);  // including first vertex at back of array
 
-    // vertices is an array of vectors
-    this.vertices = vertices;
-    this.n = vertices.length;
-    this.vertices.push(vertices[0]);  // including first vertex at back of array
+    this.setRandomColor();
 
     // Shoelace formula for area.
     this.area = 0;
     for (let i = 0; i < this.n; i++) {
-      this.area += (this.x[i] * this.y[i + 1] - this.x[i + 1] * this.y[i]) / 2;
+      this.area += (this[i].x * this[i + 1].y - this[i + 1].x * this[i].y) / 2;
     }
 
-    this.linearVelocity = new Vector(8 * Math.random() - 4, 8 * Math.random() - 4);
-    this.angularVelocity = 0.04 * Math.random() - 0.02;
+    const LIN_MAX = 4;
+    const ANG_MAX = 0.05;
+    this.linearVelocity = new Vector(LIN_MAX * (2 * Math.random() - 1),
+      LIN_MAX * (2 * Math.random() - 1));
+    this.angularVelocity = ANG_MAX * (2 * Math.random() - 1);
   }
 
+
+  // Returns random array of vertices defining a polygon.
+  static buildVertices() {
+    const n = 3 + Math.floor(3 * Math.random());
+    const vertices = [];
+    const RADIUS = 100;
+    const NOISE = 100;
+    const x = (WIDTH - 2 * RADIUS) * Math.random() + RADIUS;
+    const y = (HEIGHT - 2 * RADIUS) * Math.random() + RADIUS;
+    for (let i = 0; i < n; i++) {
+      const u = x + RADIUS * Math.cos(2 * Math.PI * i / n) + Math.random() * NOISE;
+      const v = y + RADIUS * Math.sin(2 * Math.PI * i / n) + Math.random() * NOISE;
+      vertices.push(new Vector(u, v));
+    }
+    return vertices;
+  }
+
+  // Sets color attribute with random rgb color.
   setRandomColor() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
@@ -50,19 +73,15 @@ class Polygon {
     this.color = 'rgb(' + r + ',' + g + ',' + b + ')';
   }
 
-  // Getting the x and y coordinates of the vertices for easy access
-  get x() { return this.vertices.map(v => v.x); }
-  get y() { return this.vertices.map(v => v.y); }
-
   // Formula for center of mass.
   get cm() {
     const a = 6 * this.area;
     let x = 0;
     let y = 0;
     for (let i = 0; i < this.n; i++) {
-      const term = this.x[i] * this.y[i + 1] - this.x[i + 1] * this.y[i];
-      x += (this.x[i] + this.x[i + 1]) * term;
-      y += (this.y[i] + this.y[i + 1]) * term;
+      const term = this[i].x * this[i + 1].y - this[i + 1].x * this[i].y;
+      x += (this[i].x + this[i + 1].x) * term;
+      y += (this[i].y + this[i + 1].y) * term;
     }
     return new Vector(x / a, y / a);
   }
@@ -72,21 +91,66 @@ class Polygon {
     const newCM = oldCM.plus(this.linearVelocity);
     // perform rotation about center of mass
     for (let i = 0; i <= this.n; i++) {
-      this.vertices[i] = this.vertices[i].minus(oldCM).rotate(this.angularVelocity).plus(newCM);
+      this[i] = this[i].minus(oldCM).rotate(this.angularVelocity).plus(newCM);
     }
   }
 
   draw() {
+    // drawing polygon
     c.beginPath();
-    c.moveTo(...this.vertices[0].asArray);
+    c.moveTo(...this[0].asArray);
     for (let i = 1; i < this.n; i++) {
-      console.log(...this.vertices[i].asArray);
-      c.lineTo(...this.vertices[i].asArray);
+      c.lineTo(...this[i].asArray);
     }
     c.closePath();
     c.fillStyle = this.color;
     c.fill();
+
+    // drawing center of mass
+    c.beginPath();
+    c.arc(...this.cm.asArray, 10, 0, 2 * Math.PI);
+    c.fillStyle = 'black';
+    c.fill();
   }
+
+  // Returns the normal vector to the wall.
+  normalToWall() {
+    for (let vertex of this) {
+      if (vertex.x < 0) {
+        return Vector(1, 0);
+      } else if (vertex.x > WIDTH) {
+        return Vector(-1, 0);
+      } else if (vertex.y < 0) {
+        return Vector(0, 1);
+      } else if (vertex.y > HEIGHT) {
+        return Vector(0, -1);
+      }
+    }
+    return false;
+  }
+
+  // The ray casting algorithm
+  // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+  // containsPoint(v) {
+  //   let isIn = false;
+  //   let j = this.n - 1;
+  //   for (let i = 0; i < this.n; i++) {
+  //     if (this.x[i])
+  //   }
+  //       for ( int i = 0, j = polygon.Length - 1 ; i < polygon.Length ; j = i++ )
+  //       {
+  //           if ( ( polygon[ i ].Y > p.Y ) != ( polygon[ j ].Y > p.Y ) &&
+  //                p.X < ( polygon[ j ].X - polygon[ i ].X ) * ( p.Y - polygon[ i ].Y ) / ( polygon[ j ].Y - polygon[ i ].Y ) + polygon[ i ].X )
+  //           {
+  //               inside = !inside;
+  //           }
+  //       }
+  //   let intersections = 0;
+  //   for (let i = 0; i < this.n; i++) {
+  //     let s = this.vertices[i + 1].minus(this.vertices[i]);  // an edge of poly
+  //   }
+  // }
+  
 }
 
 
@@ -108,165 +172,9 @@ class Polygon {
 //     return momentOfInertia * this.dtheta * this.dtheta;
 //   }
 
-//   // Update position and angular position.
-//   updatePositionOld() {
-//     let [a, b] = this.getLeftIntersections();
-//     if (a) {
-//       // Normal force proportional to length of intersection
-//       this.dx += 0.01 * Math.abs(a - b);
-//       drawLine(0, a, 0, b);
-//     }
 
-//     [a, b] = this.getRightIntersections();
-//     if (a) {
-//       // Normal force proportional to length of intersection
-//       this.dx -= 0.01 * Math.abs(a - b);
-//       drawLine(canvas.width, a, canvas.width, b);
-//     }
 
-//     [a, b] = this.getTopIntersections();
-//     if (a) {
-//       // Normal force proportional to length of intersection
-//       this.dy += 0.01 * Math.abs(a - b);
-//       drawLine(a, 0, b, 0);
-//     }
-
-//     [a, b] = this.getBottomIntersections();
-//     if (a) {
-//       // Normal force proportional to length of intersection
-//       this.dy -= 0.01 * Math.abs(a - b);
-//       drawLine(a, canvas.height, b, canvas.height);
-//     }
-
-//     this.x += this.dx;
-//     this.y += this.dy;
-//     this.theta += this.dtheta;
-//   }
-
-//   // Get intersection points with left wall
-//   getLeftIntersections() {
-//     let intersection1 = null;
-//     let intersection2 = null;
-//     let x0 = this.getx();
-//     let y0 = this.gety();
-//     for (let i = 0; i < this.n; i++) {
-//       this.theta += 2 * Math.PI / this.n;
-//       let x1 = this.getx();
-//       let y1 = this.gety();
-//       if (Math.sign(x0) != Math.sign(x1)) {
-//         // Found intersection with vertical wall at x = 0
-//         let intersection = y0 - x0 * (y0 - y1) / (x0 - x1);
-//         if (intersection1 === null) {
-//           intersection1 = intersection;
-//         } else {
-//           intersection2 = intersection;
-//         }
-//       }
-//       x0 = x1;
-//       y0 = y1;
-//     }
-//     return [intersection1, intersection2];
-//   }
-
-//   // Get intersection points with right wall
-//   getRightIntersections() {
-//     let intersection1 = null;
-//     let intersection2 = null;
-//     let x0 = this.getx();
-//     let y0 = this.gety();
-//     for (let i = 0; i < this.n; i++) {
-//       this.theta += 2 * Math.PI / this.n;
-//       let x1 = this.getx();
-//       let y1 = this.gety();
-//       if (Math.sign(x0 - canvas.width) != Math.sign(x1 - canvas.width)) {
-//         // Found intersection with vertical wall at x = width
-//         let intersection = y0 + (canvas.width - x0) * (y0 - y1) / (x0 - x1);
-//         if (intersection1 === null) {
-//           intersection1 = intersection;
-//         } else {
-//           intersection2 = intersection;
-//         }
-//       }
-//       x0 = x1;
-//       y0 = y1;
-//     }
-//     return [intersection1, intersection2];
-//   }
-
-//   // Get intersection points with top wall
-//   getTopIntersections() {
-//     let intersection1 = null;
-//     let intersection2 = null;
-//     let x0 = this.getx();
-//     let y0 = this.gety();
-//     for (let i = 0; i < this.n; i++) {
-//       this.theta += 2 * Math.PI / this.n;
-//       let x1 = this.getx();
-//       let y1 = this.gety();
-//       if (Math.sign(y0) != Math.sign(y1)) {
-//         // Found intersection with horizontal wall at y = 0
-//         let intersection = x0 - y0 * (x0 - x1) / (y0 - y1);
-//         if (intersection1 === null) {
-//           intersection1 = intersection;
-//         } else {
-//           intersection2 = intersection;
-//         }
-//       }
-//       x0 = x1;
-//       y0 = y1;
-//     }
-//     return [intersection1, intersection2];
-//   }
-
-//   // Get intersection points with bottom wall
-//   getBottomIntersections() {
-//     let intersection1 = null;
-//     let intersection2 = null;
-//     let x0 = this.getx();
-//     let y0 = this.gety();
-//     for (let i = 0; i < this.n; i++) {
-//       this.theta += 2 * Math.PI / this.n;
-//       let x1 = this.getx();
-//       let y1 = this.gety();
-//       if (Math.sign(y0 - canvas.height) != Math.sign(y1 - canvas.height)) {
-//         // Found intersection with horizontal wall at x = height
-//         let intersection = x0 + (canvas.height - y0) * (x0 - x1) / (y0 - y1);
-//         if (intersection1 === null) {
-//           intersection1 = intersection;
-//         } else {
-//           intersection2 = intersection;
-//         }
-//       }
-//       x0 = x1;
-//       y0 = y1;
-//     }
-//     return [intersection1, intersection2];
-//   }
-
-//   // Draw polygon centered at x, y.
-//   drawOld() {
-//     c.beginPath();
-//     for (let i = 0; i < this.n; i++) {
-//       if (i) {
-//         c.lineTo(this.getx(), this.gety());
-//       } else {
-//         c.moveTo(this.getx(), this.gety());
-//       }
-//       this.theta += 2 * Math.PI / this.n;
-//     }
-//     c.closePath();
-//     c.fillStyle = this.color;
-//     c.fill();
-//   }
-// }
-
-const vertices = [];
-for (let i = 0; i < 5; i++) {
-  vertices.push(new Vector(1000 * Math.random(), 1000 * Math.random()));
-}
-
-//const polygon = new Polygon([new Vector(100, 100), new Vector(300, 500), new Vector(500, 300)]);
-const polygon = new Polygon(vertices);
+const polygon = new Polygon();
 
 function update() {
   c.clearRect(0, 0, canvas.width, canvas.height);
@@ -274,18 +182,8 @@ function update() {
   polygon.updatePosition();
 }
 
-// Draw a yellow line segment
-function drawLine(x1, y1, x2, y2) {
-  c.beginPath();
-  c.moveTo(x1, y1);
-  c.lineTo(x2, y2);
-  c.lineWidth = 15;
-  c.strokeStyle = 'yellow';
-  c.stroke();
-}
-
 // Useful for debugging
 canvas.addEventListener('keydown', update, false);
 
 // Updating the html canvas
-//setInterval(update, 10);
+setInterval(update, 10);
