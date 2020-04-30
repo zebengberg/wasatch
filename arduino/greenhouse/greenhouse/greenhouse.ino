0fd514c31d11835c60ded778058f56a1ac3dd774
Zeb Engberg <zebengberg@gmail.com>
2020-04-30 12:52:15 -0600

    included fan timestamp based on millis()

--- arduino/greenhouse/greenhouse/greenhouse.ino
+++ arduino/greenhouse/greenhouse/greenhouse.ino
@@ -23,6 +23,7 @@
 unsigned int buttonState = 0;
 bool isButtonDown = false;
 bool fanState = LOW;
+unsigned long fanTimestamp = 0;
 
 
 void setup(){
@@ -37,11 +38,11 @@ void setup(){
 void loop(){
   setButtonState();
   blinkLED();
-  runFans();
+  timeCheck();  // determines if fans should turn on
 }
 
 
-// Read the exact voltage of the 12V battery.
+// Read the voltage of the 12V battery.
 float getVoltage() {
   float vout = analogRead(VOLTAGEPIN) * 5.0 / 1024.0;
   // Scaling based on true voltmeter reading.
@@ -50,6 +51,25 @@ float getVoltage() {
   float vin = vout * (RESISTOR1 + RESISTOR2) / RESISTOR2;
   return vin;
 }
+
+
+// Read the voltage of the 12V battery when it's powering the load.
+float getLoadVoltage() {
+  float voltage = getVoltage();
+  
+  // Only turn on fans if voltage is sufficiently high.
+  if (voltage > 12.6 && !fanState) {
+    digitalWrite(FANPIN1, HIGH);
+    digitalWrite(FANPIN2, HIGH);
+    delay(20);
+    voltage = getVoltage();
+    delay(5);
+    digitalWrite(FANPIN1, LOW);
+    digitalWrite(FANPIN2, LOW);
+  }
+  return voltage;
+}
+
 
 // Read the value from the light sensor.
 int getLight() {
@@ -65,6 +85,7 @@ void setButtonState() {
     isButtonDown = true;
     buttonState++;
     buttonState %= 3;
+    setFanState();  // called once button pressed
   } else if (!digitalRead(BUTTONPIN)) {
     isButtonDown = false;
   }
@@ -74,12 +95,12 @@ void setButtonState() {
 // Blink LED to indicate button state or low voltage.
 void blinkLED() {
   if (getVoltage() < 12.2) {
-    // Blink every 100ms to indicate low voltage.
-    int state = millis() / 100 % 2;
+    // Blink every 200ms to indicate low voltage.
+    int state = millis() / 200 % 2;
     digitalWrite(LEDPIN, state);
   } else {
-    // Blink a pattern every 1000ms.
-    int state = millis() / 100 % 10;
+    // Blink a pattern every 2000ms.
+    int state = millis() / 200 % 10;
     switch (buttonState) {
       case 0:
         digitalWrite(LEDPIN, state == 0);
@@ -95,27 +116,37 @@ void blinkLED() {
 }
 
 
-// Use transistors to power fans.
-void runFans() {
-  float voltage = getVoltage();
+// Consider calling setFanState() every 5 minutes or when millis() resets.
+void timeCheck() {
+  if (millis() == 0 || millis() > fanTimestamp + 1000ul * 60ul * 5ul) {
+    setFanState();
+  }
+}
+
+
+// Set the fanState variable and use transistors to power fans.
+void setFanState() {
+  fanTimestamp = millis();  // updating timestamp when fan was set
+  
+  float voltage = getLoadVoltage();  // expensive to call
   int light = getLight();  // values between 0 and 1023
 
   switch (buttonState) {
     case 0:
       // Power fans depending on voltage and light.
       // There is an intermediate range in which fanState not changed.
-      if (voltage > 13.4 && light > 700) {
+      if (voltage > 13.0 && light > 750) {
         fanState = HIGH;
-      } else if (voltage < 12.6 || light < 600) {
+      } else if (voltage < 12.8 || light < 650) {
         fanState = LOW;
       }
       break;
       
     case 1:
       // Power fans depending on voltage.
-      if (voltage > 13.4) {
+      if (voltage > 13.0) {
         fanState = HIGH;
-      } else if (voltage < 12.6) {
+      } else if (voltage < 12.8) {
         fanState = LOW;
       }
       break;
