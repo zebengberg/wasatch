3f8d029d749093d7cc8b95103b3c8389fa1aaf01
Zeb Engberg <zebengberg@gmail.com>
2020-06-23 14:46:29 -0600

    code for spectrophotometer, modified from Milt's

---
+++ arduino/spectrophotometer/spectrophotometer.ino
@@ -0,0 +1,61 @@
+/*
+ * Arduino used as a Spectrophotometer
+ * Written by: Milton Watts  6/11/2020 for Engineering Principles
+ * Modified by: Zeb!
+ * 
+ * Hardware configuration:
+ * One or two tri-color LED's with common cathode are used - one for lighting, and the 2nd for an optional blinking display.
+ * LED's share RED, GREEN and BLUE pins but have their own Common pin
+ * To turn on an LED the color pin should be set high while the common pin is set low.
+ * To prevent reverse current when an LED is off, set its common pin to INPUT when not in use rather than setting it HIGH.
+ * 
+ * A photoresistor is connected to an analog pin with a pull-up resistor to 5V.  
+ * 10k is a reasonable value or you can use the internal pull-up resistor in the Arduino
+ * 
+ * The code turns on each color briefly and then samples the voltage on the connected photoresistor
+ * The voltage readings are on a scale from 0 to 1023.  
+ * Output is reported on the serial port (Tools->Serial Monitor) and as blinking lights.   
+ * Count the pulses of each color to determine the 3 or 4-digit value for that color.  
+ * Zero is displayed as a single very short pulse 
+ */
+
+// Color pins shared by all LEDs 
+#define RED_PIN 9
+#define GREEN_PIN 10
+#define BLUE_PIN 11
+
+// Photoresistor connected to an Analog pin using either internal or external pullup resistor to 5V
+#define PR_PIN A0
+
+// Blinking speeds
+#define DELAY 500
+#define BAUDRATE 9600
+
+void setup() {
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  pinMode(PR_PIN, INPUT);  // Assumes a 10k resistor to 5V with photo-resistor to GND
+ 
+  Serial.begin(BAUDRATE);
+  Serial.println("Red\tGreen\tBlue");
+}
+
+void loop() {
+  measurePR(RED_PIN);
+  measurePR(GREEN_PIN);
+  measurePR(BLUE_PIN);
+}
+
+void measurePR(int color) {
+  digitalWrite(color, HIGH);
+  delay(DELAY);
+  int sensorValue = analogRead(PR_PIN);
+  sensorValue = 1023 - sensorValue;
+  digitalWrite(color, LOW);
+  Serial.print(sensorValue);
+  Serial.print("\t");
+  if (color == BLUE_PIN) {
+    Serial.print("\n");
+  }
+}
